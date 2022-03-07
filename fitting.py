31912a974f7192f50b24ca8b2f38ac4dc2fba34b
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2022-03-07 20:18:15 +0900

    :sparkles: Feature : curve fitting

---
+++ fitting.py
@@ -0,0 +1,60 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+file_name = 'csv/GJM1555C1HR50BB01_InProduction.csv'
+freq_max = 4.5e9
+freq_min = 0.5e9
+
+
+with open(file_name) as f:
+    reader = csv.reader(f)
+
+    # skip sharp '#'
+    header = reader.__next__()
+
+    while header[0][0] == '#':
+        header = reader.__next__()
+
+    print(header)
+
+    freq = np.array([])
+    cap  = np.array([])
+
+    for row in reader:
+        freq = np.append(freq, float(row[0]))
+        cap  = np.append(cap , float(row[1]))
+
+# pick up range
+i_max = len(freq)-1
+i_min = 0
+
+while freq[i_max] >= freq_max:
+    i_max += -1
+
+while freq[i_min] <= freq_min:
+    i_min += 1
+
+freq = freq[i_min:i_max]
+cap  = cap[i_min:i_max]
+
+# convert unit
+freq /= 1e9
+cap  *= 1e12
+
+# fit function
+def func(f, a, b, c, d):
+    return a + b * np.exp(np.multiply(f, c) - d )
+
+# curve fitting
+popt, pcov = curve_fit(func, freq, cap)
+
+print(popt)
+
+plt.scatter(freq, cap, s=5, c='blue', zorder=2)
+plt.plot(freq, func(freq, *popt), lw=4, c='red', zorder=1)
+plt.xlabel('Frequency [GHz]')
+plt.ylabel('Capacity [pF]')
+plt.show()
