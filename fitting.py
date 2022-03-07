744c100abf601462e03af53fe14dd5ff3d44d440
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2022-03-07 20:30:01 +0900

    :sparkles: Feature : plot details

--- fitting.py
+++ fitting.py
@@ -1,3 +1,5 @@
+from cProfile import label
+import sys
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,8 +7,19 @@ from scipy.optimize import curve_fit
 
 
 file_name = 'csv/GJM1555C1HR50BB01_InProduction.csv'
-freq_max = 4.5e9
 freq_min = 0.5e9
+freq_max = 4.5e9
+
+if len(sys.argv) == 3:
+    file_name = sys.argv[1]
+    freq_min  = sys.argv[2]
+    freq_max  = sys.argv[3]
+
+
+print("================================================================")
+print("- File path : {}".format(file_name))
+print("- Frequency : {} ~ {} [GHz]".format(freq_min/1e9, freq_max/1e9))
+print("================================================================")
 
 
 with open(file_name) as f:
@@ -18,7 +31,6 @@ with open(file_name) as f:
     while header[0][0] == '#':
         header = reader.__next__()
 
-    print(header)
 
     freq = np.array([])
     cap  = np.array([])
@@ -51,10 +63,11 @@ def func(f, a, b, c, d):
 # curve fitting
 popt, pcov = curve_fit(func, freq, cap)
 
-print(popt)
 
 plt.scatter(freq, cap, s=5, c='blue', zorder=2)
-plt.plot(freq, func(freq, *popt), lw=4, c='red', zorder=1)
+plt.plot(freq, func(freq, *popt), lw=4, c='red', zorder=1, label='{:.2E} + {:.2E}*exp(freq[GHz] * {:.2E} - {:.2E})'.format(*popt))
+plt.title(file_name.split('/')[-1])
 plt.xlabel('Frequency [GHz]')
 plt.ylabel('Capacity [pF]')
+plt.legend()
 plt.show()
